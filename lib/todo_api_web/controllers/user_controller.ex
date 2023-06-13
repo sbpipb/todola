a45ba0e3ff8c21f482eb26a17754c4b19afd19f7
sbpipb <sbpipb>
2023-06-13 09:08:30 +0800

    Adds user registration controller action - working endpoint - basic message response

--- lib/todo_api_web/controllers/user_controller.ex
+++ lib/todo_api_web/controllers/user_controller.ex
@@ -1,9 +1,30 @@
 defmodule TodoApiWeb.UserController do
 
   use TodoApiWeb, :controller
+  # import TodoApi.Accounts.User
+  alias TodoApi.Accounts
 
-  def register(conn, params) do
-    # %{ "username" => username, "password" => password } = params
-    json(conn, %{ success: true })
+  # action_fallback TodoApiWeb.MyFall
+
+
+  # def index(conn, _params) do
+  #   users = User.list_users()
+  #   render(conn, :index, users: users)
+  # end
+
+
+  def register(conn, %{"user" => params}) do
+
+    case Accounts.User.register_user(params) do
+      {:ok, user } ->
+        conn |> json(%{ message: "User created", username: user.username, user: user.id})
+
+      {:error, _changeset } ->
+        # TODO: handle better error messaging by parsing changeset errors
+
+        conn
+        |> put_status(:bad_request)
+        |> json(%{ message: "Failed to register"})
+    end
   end
 end
