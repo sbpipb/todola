0e663a8ab4c33dcafc444e24270a300e26fd41cb
sbpipb <sbpipb>
2023-06-13 09:12:20 +0800

    Updates Accounts User schema - Adds register user

--- lib/todo_api/accounts/user.ex
+++ lib/todo_api/accounts/user.ex
@@ -2,16 +2,45 @@ defmodule TodoApi.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias TodoApi.Accounts.User
+
   schema "users" do
     field :username, :string
+    field :password, :string
 
     timestamps()
   end
 
+  @spec changeset(
+          {map, map}
+          | %{
+              :__struct__ => atom | %{:__changeset__ => map, optional(any) => any},
+              optional(atom) => any
+            },
+          :invalid | %{optional(:__struct__) => none, optional(atom | binary) => any}
+        ) :: Ecto.Changeset.t()
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:username])
-    |> validate_required([:username])
+    |> cast(attrs, [:username, :password])
+    |> validate_required([:username, :password])
+    |> unique_constraint(:username)
+
   end
+
+  @spec register_user(:invalid | %{optional(:__struct__) => none, optional(atom | binary) => any}) ::
+          any
+  def register_user(attrs) do
+    %User{}
+    |> User.changeset(attrs)
+    |> TodoApi.Repo.insert
+  end
+
+  # import Ecto.Query, only: [from: 2]
+
+
+  # def list_users() do
+  #   query = from u in TodoApi.Accounts.User, select: u.username
+  #   TodoApi.Repo.all(query)
+  # end
 end
