bac976ce4f26e8575bd59ef1156ada79c0afb212
sbpipb <julianmacmang@gmail.com>
2024-09-25 22:33:26 +0800

    credo lint fixes

--- lib/todo_api_web/controllers/list_controller.ex
+++ lib/todo_api_web/controllers/list_controller.ex
@@ -1,19 +1,19 @@
 defmodule TodoApiWeb.ListController do
   use TodoApiWeb, :controller
 
-  def create(conn, %{ "list" => list_params } ) do
+  def create(conn, %{"list" => list_params } ) do
 
     case TodoApi.Lists.create_list(list_params) do
       {:ok, list} ->
-        conn |> json(%{ message: "List created", title: list.title, id: list.id, description: list.description})
+        conn |> json(%{message: "List created", title: list.title, id: list.id, description: list.description})
 
       {:error, changeset } ->
-        error_list = changeset.errors 
-                      |> Enum.map(fn {attr, { msg, _constraints} = v} -> 
+        error_list = changeset.errors
+                      |> Enum.map(fn {attr, {msg, _constraints} = _v} ->
                         "#{attr} #{msg}"
                       end)
 
-        conn |> json(%{ message: "failed to create", errors: error_list})
+        conn |> json(%{message: "failed to create", errors: error_list})
     end
   end
 end
