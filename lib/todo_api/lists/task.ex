92e13b2c8c8103dfc37b9b5f36ccc93d00952644
sbpipb <julianmacmang@gmail.com>
2024-09-26 15:03:02 +0800

    adds move_count limit when moving task

--- lib/todo_api/lists/task.ex
+++ lib/todo_api/lists/task.ex
@@ -8,21 +8,30 @@ defmodule TodoApi.Lists.Task do
     field :user_id, :id
     field :list_id, :id
     field :order_number, :float, default: 0.0
+    field :move_count, :integer, default: 50
     timestamps()
   end
 
   @doc false
   def changeset(task, attrs) do
     task
-    |> cast(attrs, [:title, :completed, :order_number])
+    |> cast(attrs, [:title, :list_id, :user_id, :completed, :order_number, :move_count])
   end
 
-  def create_changeset(task, attrs) do
+  def create_task_changeset(task, attrs) do
     task
     |> changeset(attrs)
-    |> validate_required([:title, :completed])
+    |> validate_required([:title, :completed, :list_id])
   end
 
+  @spec update_changeset(
+          {map(), map()}
+          | %{
+              :__struct__ => atom() | %{:__changeset__ => any(), optional(any()) => any()},
+              optional(atom()) => any()
+            },
+          :invalid | %{optional(:__struct__) => none(), optional(atom() | binary()) => any()}
+        ) :: Ecto.Changeset.t()
   def update_changeset(task, attrs) do
     task
     |> changeset(attrs)
