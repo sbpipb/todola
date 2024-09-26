fdeb7faca47cda9b241a8ae611a3a31e735f5a92
sbpipb <julianmacmang@gmail.com>
2024-09-26 15:03:15 +0800

    delete duplicate migration

--- priv/repo/migrations/20240926020307_create_tasks.exs
+++
@@ -1,9 +0,0 @@
-defmodule TodoApi.Repo.Migrations.CreateTasks do
-  use Ecto.Migration
-
-  def change do
-    alter table(:tasks) do
-      add :order_number, :float, default: 0.0
-    end
-  end
-end
